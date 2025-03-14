c69745b1f7946fcc64332b36eab059608dd7d900
Sandesh Kumar <sandesh.kumar@databricks.com>
2025-03-14 11:23:54 +0530

    first commit2

---
+++ tests/hello_tests.py
@@ -0,0 +1,12 @@
+import sys, os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
+
+from src.math_ops import add,sub
+
+def test_add():
+    assert add(2,3)==5
+    assert add(-1,1)==0
+    
+def test_sub():
+    assert sub(5,3)==2
+    assert sub(4,3)==1
